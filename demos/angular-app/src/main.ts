3232093a2b5c1bc4537dd2e3d50744564c29ed9f
b4rtaz <b4rtaz@gmail.com>
2023-07-23 12:33:38 +0200

    prettier fix.

--- demos/angular-app/src/main.ts
+++ demos/angular-app/src/main.ts
@@ -2,8 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-window.addEventListener('load', () => {
-	platformBrowserDynamic()
-		.bootstrapModule(AppModule)
-		.catch(err => console.error(err));
-}, false);
+window.addEventListener(
+	'load',
+	() => {
+		platformBrowserDynamic()
+			.bootstrapModule(AppModule)
+			.catch(err => console.error(err));
+	},
+	false
+);
