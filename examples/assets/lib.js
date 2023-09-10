4d24e21e2ecc7ccca1f2e281e9a57d6c98514849
Bart Tadych <b4rtaz@gmail.com>
2023-09-11 00:42:16 +0200

    0.15.2. (#81)

--- examples/assets/lib.js
+++ examples/assets/lib.js
@@ -19,7 +19,7 @@ function embedStylesheet(url) {
 
 const baseUrl = isTestEnv()
 	? '../designer'
-	: '//cdn.jsdelivr.net/npm/sequential-workflow-designer@0.14.2';
+	: '//cdn.jsdelivr.net/npm/sequential-workflow-designer@0.15.2';
 
 embedScript(`${baseUrl}/dist/index.umd.js`);
 embedStylesheet(`${baseUrl}/css/designer.css`);
